ea588f8fcba4df009b20735d9b3a50fbfb9a6999
Egor <meshceryakoff@mail.ru>
2019-07-09 21:17:38 +0700

    09.07

--- shift-frontend/js/main.js
+++ shift-frontend/js/main.js
@@ -1,19 +1,178 @@
-const createUser = function () {
+const autoMoveTimer = 30;//Время на ход в секундах, по истечению которого происходит автоход
+const checkTimer = 500;//Таймер проверки комнаты в миллисекундах
+var roomId;
+var userId;
+var roundsCount = 0;
+//var userId;
+const button_ROCK = document.querySelector('#ROCK');
+const button_SCISSORS = document.querySelector('#SCISSORS');
+const button_PAPER = document.querySelector('#PAPER');
 
-    const userName = "IvanIvan";
 
-    createRequest({ path: `/api/v001/user/create`, method: "POST" }, {}, {})
-    .then(response => {
-        toggleClass(".add-user", "loading");
-        console.log("Юзер добавлен", response);
-    })
-    .catch(() => {
-        toggleClass(".add-user", "loading");
-        console.log("Не удалось добавить юзера");
-    });
+const createUser = function (userName) {
+    //Запрос на создание юзера
+    createRequest({ path: `/api/v001/user/create/fast`, method: "POST" }, {}, {}, "userName", userName)
+        //Запрос на создание комнаты,  передаём данные дальше
+        .then(response => {
+            toggleClass(".add-user", "loading");
+            console.log("Юзер добавлен", response);
+            if (response.free) {
+                userId = response.usersId[0];
+            }
+            else {
+                userId = response.usersId[1];
+            }
+            roomId = response.roomId;
+            //return createRequest({ path: `/api/v001/room/create`, method: "POST" }, {}, {}, "userId", userId);
+        },
+        () => {
+            console.log("Ошибка при создании юзера");
+        })
+        //Отрисовка страницы игры
+        .then(function() {
+            //console.log("roomID", response_room.roomId);
+            //return //Функция игры(response_room.roomId);
+
+            //нарисовали комнату, кнопки
+            //var timerId = setTimeout(autoMove, autoMoveTimer*1000); //таймер автохода
+            //clearTimeout(timerId);
+
+            button_ROCK.addEventListener("click", event => {
+                event.preventDefault();
+                setMove(roomId, userId, "ROCK")
+            });
+            button_SCISSORS.addEventListener("click", event => {
+                event.preventDefault();
+                setMove(roomId, userId, "SCISSORS")
+            });
+            button_PAPER.addEventListener("click", event => {
+                event.preventDefault();
+                setMove(roomId, userId, "PAPER")
+            });
+
+            //при нажатии на кнопку делать запрос и менять флаг автохода
+            //проверять состояние комнаты
+        },
+        () => {
+            console.log("Ошибка при создании комнаты");
+        });
 }
 
-createUser();
+createUser("Ivan (front)");
+
+
+const autoMove = function () {
+    alert('Время на ход истекло');
+    setMove(0);
+}
+
+const setMove = function (roomId, userId, userMove) {
+    button_ROCK.disabled = true;
+    button_SCISSORS.disabled = true;
+    button_PAPER.disabled = true;
+
+    createRequest({ path: `/api/v001/room/${roomId}/${userId}/move`, method: "PUT" }, {}, {}, "userMove", userMove)
+        .then(response => {
+
+            var timerId = setTimeout(function () { checkRoom(roomId) }, checkTimer); //Проверять комнату каждые checkTimer миллисекунд
+
+            //(function () {
+            //    clearInterval(timerId);
+            //    alert('Нет ответа от сервера. Возможно, противник вышел');
+            //}, autoMoveTimer*1000+10000); //Остановить проверку комнаты,если слишком долго нет ответа
+
+            console.log("Сделан ход: ", userMove, response);
+        })
+        .catch(() => {
+            console.log("Не удалось сделать ход:", response);
+        });
+}
+
+const checkRoom = function (roomId) {
+    createRequest({ path: `/api/v001/room/${roomId}/state`, method: "GET" })
+        .then(response => {
+            if (response.free) {
+                console.log("Ожидаем противника");
+                setTimeout(function () { checkRoom(roomId) }, checkTimer); //Проверять комнату каждые checkTimer миллисекунд
+            }
+            else {
+                console.log("Есть противник");
+                if (response.roundNumber >= roundsCount + 1) {
+                    console.log("Противник сделал ход");
+                    button_ROCK.disabled = false;
+                    button_SCISSORS.disabled = false;
+                    button_PAPER.disabled = false;
+
+                    if (response.resultsRounds[roundsCount] == userId) alert('Вы победили');
+                    else alert('Вы проиграли');
+
+                    roundsCount++;
+
+                    if (roundsCount >= 11) {
+                        if (response.winner == userId) alert('Вы победили');
+                        else alert('Вы проиграли');
+                        return;
+                    }
+                }
+                else {
+                    console.log("Ожидаем ход противника");
+                    setTimeout(function () { checkRoom(roomId) }, checkTimer); //Проверять комнату каждые checkTimer миллисекунд
+                }
+            }
+            //var timerId = setTimeout(function () { checkRoom() }, checkTimer); //Проверять комнату каждые checkTimer миллисекунд
+
+            //(function () {
+            //    clearInterval(timerId);
+            //    alert('Нет ответа от сервера. Возможно, противник вышел');
+            //}, autoMoveTimer*1000+10000); //Остановить проверку комнаты,если слишком долго нет ответа
+
+            console.log("Состояние комнаты: ", response);
+        })
+        .catch(() => {
+            console.log("Не удалось получить состояние комнаты");
+        });
+}
+
+/*
+const createRoom = function (userId) {
+    createRequest({ path: `/api/v001/room/create`, method: "POST" }, {}, {}, "userId", userId)
+        .then(response => {
+            toggleClass(".add-room", "loading");
+            console.log("Комната создана", response);
+            var roomId = response.roomId;
+            console.log("roomID", roomId);
+        })
+        .catch(() => {
+            toggleClass(".add-room", "loading");
+            console.log("Не удалось создать комнату");
+        });
+}*/
+
+//createRoom();
+
+//setTimeout(createRoom(), 5000);
+
+/*
+const setMove = document.querySelector("#resaultClick");
+setMove.addEventListener("submit", event => {
+    event.preventDefault();
+
+    const data = getFieldData(event.target);
+    console.log("main", "data", data);
+
+    //toggleClass(".add-book", "loading");
+
+    createRequest({ path: `/api/v001/room/${ roomID }/move`, method: "POST" }, {}, data)
+        .then(response => {
+            toggleClass(".add-book", "loading");
+            console.log("Книга добавлена", response);
+        })
+        .catch(() => {
+            toggleClass(".add-book", "loading");
+            console.log("Не удалось добавить книгу");
+        });
+});
+
 
 // Пример получения и вывода списка книг
 const renderBook = book => `
@@ -87,3 +246,4 @@ userSelector.addEventListener('change', event => {
   userId = event.target.value;
   getAllBooks();
 });
+*/
--- shift-frontend/js/utils.js
+++ shift-frontend/js/utils.js
@@ -1,40 +1,41 @@
-let userId = document.querySelector('.select_control-user').value;
+//let userId = document.querySelector('.select_control-user').value;
+//const HOST = 'https://5d1daff03374890014f005f1.mockapi.io';
 
 const compileUrl = (url, params) => {
-  const resultArr = [];
-  const options = Object.assign({}, params);
+    const resultArr = [];
+    const options = Object.assign({}, params);
 
-  const pathArr = url.split("/");
+    const pathArr = url.split("/");
 
-  pathArr.forEach(item => {
-    if (item[0] === ":") {
-      if (item[item.length - 1] === "?") {
-        const key = item.substring(1, item.length - 1);
-        if (options[key]) {
-          resultArr.push(options[key]);
-          delete options[key];
-        }
-      } else {
-        const key = item.substring(1);
-        if (options[key]) {
-          resultArr.push(options[key]);
-          delete options[key];
+    pathArr.forEach(item => {
+        if (item[0] === ":") {
+            if (item[item.length - 1] === "?") {
+                const key = item.substring(1, item.length - 1);
+                if (options[key]) {
+                    resultArr.push(options[key]);
+                    delete options[key];
+                }
+            } else {
+                const key = item.substring(1);
+                if (options[key]) {
+                    resultArr.push(options[key]);
+                    delete options[key];
+                } else {
+                    console.error(new Error("can not find parameter"));
+                }
+            }
         } else {
-          console.error(new Error("can not find parameter"));
+            resultArr.push(item);
         }
-      }
-    } else {
-      resultArr.push(item);
-    }
-  });
+    });
 
-  let resultString = resultArr.join("/");
+    let resultString = resultArr.join("/");
 
-  Object.keys(options).forEach((key, index) => {
-    resultString += `${index === 0 ? "?" : "&"}${key}=${options[key]}`;
-  });
+    Object.keys(options).forEach((key, index) => {
+        resultString += `${index === 0 ? "?" : "&"}${key}=${options[key]}`;
+    });
 
-  return resultString;
+    return resultString;
 };
 
 /**
@@ -43,71 +44,76 @@ const compileUrl = (url, params) => {
  * @param  {Object|undefined} body         [description]
  * @return {Promise}              [description]
  */
-const createRequest = (options, queryOptions, body) => {
-  const requestUrl = compileUrl(options.path, queryOptions);
+const createRequest = (options, queryOptions, body, headerField, headerValue) => {
+    const requestUrl = compileUrl(options.path, queryOptions);
+    console.log('headerField', headerField);
+    console.log('headerValue', headerValue);
 
-  return fetch(requestUrl, {
-    headers: new Headers({
-            Accept: "application/json",
-            "Content-Type": "application/json",
-            "userName": "Front"
-        }),
-    method: options.method || "GET",
-    body: body ? JSON.stringify(body) : undefined
-  }).then(response => {
-    if (response.status === 200) {
-      return response.json();
-    } else {
-      throw response.status;
-    }
-  });
+    let headers = new Headers({
+        Accept: "application/json",
+        "Content-Type": "application/json"
+    });
+    headers.append(headerField, headerValue);
+
+    return fetch(requestUrl, {
+        headers: headers,
+        method: options.method || "GET",
+        body: body ? JSON.stringify(body) : undefined
+    }).then(response => {
+        if (response.status === 200) {
+            return response.json();
+        } else {
+            throw response
+            ;
+        }
+    });
 };
 
 const toggleClass = (element, className, needAdd) => {
-  if (typeof element === "string") {
-    element = document.querySelector(element);
-  }
+    if (typeof element === "string") {
+        element = document.querySelector(element);
+    }
 
-  if (element) {
-    element.classList.toggle(
-      className,
-      typeof needAdd !== "undefined" ? needAdd : undefined
-    );
-  }
+    if (element) {
+        element.classList.toggle(
+            className,
+            typeof needAdd !== "undefined" ? needAdd : undefined
+        );
+    }
 };
 
 const getFieldValue = element => {
-  if (element.tagName === "SELECT" && element.multiple) {
-    const values = [];
+    if (element.tagName === "SELECT" && element.multiple) {
+        const values = [];
 
-    Array.from(element.selectedOptions).forEach(option => {
-      values.push(option.value);
-    });
+        Array.from(element.selectedOptions).forEach(option => {
+            values.push(option.value);
+        });
 
-    return values;
-  }
+        return values;
+    }
 
-  switch (element.getAttribute("type")) {
-    case "radio":
-      if (element.checked) {
-        return element.value;
-      }
-      break;
-    case "checkbox":
-      return element.checked;
-    default:
-      return element.value;
-  }
+    switch (element.getAttribute("type")) {
+        case "radio":
+            if (element.checked) {
+                return element.value;
+            }
+            break;
+        case "checkbox":
+            return element.checked;
+        default:
+            return element.value;
+    }
 };
 
 const FIELD_SELECTORS = "input, textarea, select, radio";
 const getFieldData = formElement => {
-  const elements = formElement.querySelectorAll(FIELD_SELECTORS);
-  const result = {};
+    const elements = formElement.querySelectorAll(FIELD_SELECTORS);
+    const result = {};
 
-  elements.forEach(element => {
-    result[element.name] = getFieldValue(element);
-  });
+    elements.forEach(element => {
+        result[element.name] = getFieldValue(element);
+    });
 
-  return result;
+    return result;
 };
